bfdcd4dbeab68c441fdb47146eac157c6128b397
Braian Dickson <braiandickson@gmail.com>
2017-08-17 10:46:58 -0300

    Adds the router's config.

--- wbooks/src/app/index.js
+++ wbooks/src/app/index.js
@@ -3,13 +3,9 @@ import React from 'react';
 import './styles.css';
 import Dashboard from './screens/dashboard';
 
-
-const App = () => {
-  return (
-    <div className="App">
-      <Dashboard />
-    </div>
-  );
-}
+const App = () =>
+  <div className="App">
+    <Dashboard />
+  </div>;
 
 export default App;
--- wbooks/src/app/screens/dashboard/index.js
+++ wbooks/src/app/screens/dashboard/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Home from './screens/Home';
 import './styles.css';
 
-const Dashboard = () => <Home />;
+const Dashboard = () =>
+  <div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  </div>;
 
 export default Dashboard;
